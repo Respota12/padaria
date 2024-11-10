6c6711751b8a9c38ff4fbb9d14b2b539ef4e6dbc
Respota12 <hasselsergio@gmail.com>
2024-11-10 16:28:45 -0300

    criação de pagina de cadastro de usuário

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pathlib import Path
+
 
 # Importação da view da aplicação 'todos'
 from todos.views import todo_list
@@ -8,7 +9,6 @@ from todos.views import todo_list
 # Definição das URLs do projeto
 urlpatterns = [
     path("admin/", admin.site.urls),  # URL para acessar o admin do Django
-    path(
-        "", todo_list, name="todo_list"
-    ),  # URL raiz para a lista de todos, com nome nomeado
+    path("", todo_list, name="todo_list"),  # URL raiz para a lista de todos, com nome nomeado
+    path("cadastrar", include('user.urls'))
 ]
