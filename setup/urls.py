ac7c7dca9eb78cbe773506b279ef65b12840261b
daniel <danielfsilva.2019.2@gmail.com>
2024-11-30 17:39:32 -0300

    Criação urls para novas páginas

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from user.views import cadastrar, logar, inicial
+from user.views import cadastrar, logar, inicial, logout_view
 from django.urls import path
 from todos.views import (
     TodoListView, 
@@ -8,6 +8,10 @@ from todos.views import (
     TodoUpdateView, 
     TodoDeleteView, 
     TodoCompleteView,
+)
+from product.views import (
+    listar_produtos,
+    cadastrar_produto
 )
 
 urlpatterns = [
@@ -19,5 +23,8 @@ urlpatterns = [
     path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete"),
     path("cadastrar/", cadastrar, name="cadastrar"),
     path("login/", logar, name="login"),
-    path("", inicial, name="inicial")
+    path("", inicial, name="inicial"),
+    path('logout/', logout_view, name='logout'),
+    path('produtos/', listar_produtos, name='listar_produtos'),
+    path('produtos/cadastrar/', cadastrar_produto, name='cadastrar_produto'),
 ]
